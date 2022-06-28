305f4dffdbbf7fc2063a6210ffa396f8273ce88a
jiyeon-agnes-lee <jy.agnes.lee@gmail.com>
2022-06-28 23:34:38 +0900

    update: 로직 변경에 따른 메시지 기능 수정 (#28)

--- api/src/main/java/com/bouquet/api/icon/exception/IconNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bouquet.api.icon.exception;
-
-import javax.persistence.EntityNotFoundException;
-
-public class IconNotFoundException extends EntityNotFoundException {
-    public IconNotFoundException() {
-        super("존재하지 않는 아이콘입니다.");
-    }
-}
