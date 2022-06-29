9ce94cad85f31e44f814a6b801b123ea06b01b40
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-06-29 17:45:40 +0900

    User 탈퇴 기능 구현

--- api/src/main/java/com/bouquet/api/user/dto/UserResponse.java
+++ api/src/main/java/com/bouquet/api/user/dto/UserResponse.java
@@ -1,8 +1,25 @@
 package com.bouquet.api.user.dto;
 
+import com.bouquet.api.post.dto.Post;
+import com.bouquet.api.post.dto.PostResponse;
 import lombok.*;
 
 public class UserResponse {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class OnlyId {
+        private long id;
+        public static UserResponse.OnlyId build(User user) {
+            return UserResponse.OnlyId.builder()
+                    .id(user.getId())
+                    .build();
+        }
+    }
+
+
 
     @Getter
     @Builder
