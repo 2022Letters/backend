7261ae12db0509a7e9d8487ecd751d46547651a4
jiyeon-agnes-lee <jy.agnes.lee@gmail.com>
2022-07-07 20:56:09 +0900

    feat: CORS 설정(#73)

--- api/src/main/java/com/bouquet/api/config/WebConfig.java
+++ api/src/main/java/com/bouquet/api/config/WebConfig.java
@@ -1,22 +1,27 @@
-//package com.bouquet.api.config;
-//
-//
+package com.bouquet.api.config;
+
+
 //import com.bouquet.api.interceptor.JWTInterceptor;
 //import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.*;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-//    }
-//
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
@@ -32,5 +37,5 @@
 //                .addPathPatterns("/api/msg");
 //
 //    }
-//
-//}
+
+}
