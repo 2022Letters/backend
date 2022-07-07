08da9253e9ebcd5cfdcc1c5156093a0d5a5f85ff
jiyeon-agnes-lee <jy.agnes.lee@gmail.com>
2022-07-08 00:49:02 +0900

    update: *

--- api/src/main/java/com/bouquet/api/config/WebConfig.java
+++ api/src/main/java/com/bouquet/api/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
