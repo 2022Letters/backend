06aecd888c721e2133d7be2b1ffbfbdf02f35d69
hyungi4608 <hyungi4608@gmail.com>
2022-08-23 23:51:30 +0900

    feat:cors 처리

--- api/src/main/java/com/bouquet/api/config/WebConfig.java
+++ api/src/main/java/com/bouquet/api/config/WebConfig.java
@@ -14,9 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 
