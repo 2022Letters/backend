607614b419bbec1484dcec18e5982fe463c901f9
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-08-30 23:18:43 +0900

    refactor: 메시지 서비스 주석 추가 (#104)

--- api/src/main/java/com/bouquet/api/message/service/MessageService.java
+++ api/src/main/java/com/bouquet/api/message/service/MessageService.java
@@ -14,18 +14,25 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MessageService {
+    
     private final MessageRepository messageRepository;
     private final PostRepository postRepository;
+    
+    // 메시지 생성
     public MessageResponse.OnlyId create(MessageRequest.Create request) {
         Post post = postRepository.findById(request.getPostId()).orElseThrow(PostNotFoundException::new);
         Message message = Message.create(request, post);
         Message savedMessage = messageRepository.save(message);
         return MessageResponse.OnlyId.build(savedMessage);
     }
+    
+    // 매시지 단건 조회
     public MessageResponse.GetMessage getMessage(Long messageId) {
         Message message = messageRepository.findById(messageId).orElseThrow(MessageNotFoundException::new);
         return MessageResponse.GetMessage.build(message);
     }
+    
+    // 메시지 삭제
     public MessageResponse.OnlyId delete(Long messageId) {
         Message message = messageRepository.findById(messageId).orElseThrow(MessageNotFoundException::new);
         messageRepository.deleteById(messageId);
